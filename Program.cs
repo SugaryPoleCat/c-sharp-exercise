a0b639db42545e9bde7f8135481ff62e8bf4c436
Sugar <sugarypolecat@gmail.com>
2021-10-28 09:35:59 +0200

    updated a little so console pauses and waits for input.

--- Program.cs
+++ Program.cs
@@ -13,9 +13,13 @@ namespace CSharp_Training
             // concat the string
             Console.WriteLine("There once was a man named " + characterName);
             Console.WriteLine("He was " + characterAge + " years old");
+
+            // if we want to change the name sudddenlly
             Console.WriteLine("He really liked the name " + characterName);
             Console.WriteLine("But didn't like being " + characterAge);
-            Console.WriteLine("Hello World!");
+
+            // waits for iunput = pauses the console.
+            Console.ReadLine();
         }
     }
 }
