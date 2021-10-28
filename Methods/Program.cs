a70ab9c052735f820ed3bbe6b18aa5c768844fe2
Sugar <sugarypolecat@gmail.com>
2021-10-28 12:57:49 +0200

    working on methods or aka functions. they are kinda the same

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -4,8 +4,26 @@ namespace Methods
 {
     class Program
     {
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + "/nYou are: " + age);
+        }
+        // this is a method!
         static void Main(string[] args)
         {
+            // call the method SayHi
+            Console.Write("Your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            SayHi(name, age);
+
+            // exercise: make it print out your name;
+            // based on what you learned in MadLib, CSHarpBasics, Calculator.
+            // You need: a string. 
+
+            // MAKE SOMETHING LKE THAT FOR SEB
+            // what i need to do is make a repo, where he can do that .
             Console.ReadLine();
         }
     }
