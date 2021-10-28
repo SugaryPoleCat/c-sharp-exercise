b8832927b57d166682a4556f4238cb3d238af3ea
Sugar <sugarypolecat@gmail.com>
2021-10-28 12:12:46 +0200

    beginning of madlib

---
+++ MadLib.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Training
+{
+    class MadLib
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Roses are red");
+            Console.WriteLine("Violets are blue");
+            Console.WriteLine("I love you");
+            Console.ReadLine();
+        }
+    }
+}
