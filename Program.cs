448ea7ddcbed2940ec92ff4bc89bdacdc5524e4c
Sugar <sugarypolecat@gmail.com>
2021-10-28 07:36:39 +0200

    init
    initial comit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharp_Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("There once was a man named John");
+            Console.WriteLine("He was 35 years old");
+            Console.WriteLine("He really liked the name John");
+            Console.WriteLine("But didn't like being 35");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
