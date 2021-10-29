88f38fe4b10217a5d35aa57526a2df8c1ea39353
Sugar <sugarypolecat@gmail.com>
2021-10-29 12:55:57 +0200

    adding guessing game

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
