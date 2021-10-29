5bbb27e7edd03ad3a77378e6b3cd3d0c9c75116a
Sugar Polecat <sugarypolecat@gmail.com>
2021-10-29 13:10:08 +0200

    making a guessing game, will be exercise for seb

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -6,12 +6,34 @@ namespace GuessingGame
     {
         public static void Main(string[] args)
         {
-            string word = "bajs";
+            string toGuess = "Cukier";
             string guess = "";
-            do
-            {
+            int tries = 0;
+            int limit = 3;
+            bool oog = false;
 
+            while (guess != toGuess && !oog)
+            {
+                if (tries < limit)
+                {
+                    Console.Write("Guess the word: ");
+                    guess = Console.ReadLine();
+                    tries++;
+                } else
+                {
+                    oog = true;
+                }
             }
+            switch (oog) {
+                case true:
+                    Console.WriteLine("You lose!");
+                    break;
+                default:
+                    Console.WriteLine("You win!");
+                    break;
+            }
+
+            Console.ReadLine();
         }
     }
 }
