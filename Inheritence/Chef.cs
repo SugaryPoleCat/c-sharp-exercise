45deb87ab983e1c102fb46055f41108745bb65b1
Sugar Polecat <sugarypolecat@gmail.com>
2021-10-29 17:24:34 +0200

    wroking on inheritence, done with skeleton class

--- Inheritence/Chef.cs
+++ Inheritence/Chef.cs
@@ -8,5 +8,26 @@ namespace Inheritence
 {
     class Chef
     {
+        /// <summary>
+        /// Tell the chef to make a chicken.
+        /// </summary>
+        public void MakeChicken()
+        {
+            Console.WriteLine("The chef makes chicken");
+        }
+        /// <summary>
+        /// Tell the chef to make a salad
+        /// </summary>
+        public void MakeSalad()
+        {
+            Console.WriteLine("The chef makes salad");
+        }
+        /// <summary>
+        /// Tell teh chef to make a special dish.
+        /// </summary>
+        public virtual void MakeSpecialDish()
+        {
+            Console.WriteLine("The chef makes BBQ ribs");
+        }
     }
 }
